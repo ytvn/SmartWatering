bd88097fd5c185b2b36affa890c5d1c9dd4cdaae
ytvn <ytvn.it@gmail.com>
2020-07-05 10:27:20 +0700

    manage user role

--- SmatWatering/BookListMVC/Migrations/20200704161500_add-city.Designer.cs
+++ SmatWatering/BookListMVC/Migrations/20200704161500_add-city.Designer.cs
@@ -4,14 +4,16 @@ using BookListMVC.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace SmartWatering.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200704161500_add-city")]
+    partial class addcity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
